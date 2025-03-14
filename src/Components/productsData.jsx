543a2e68c00836eeba0d699ff4c36829685cec9b
Oluwaseun Oloto <oloto.oluwaseun@gmail.com>
2025-03-14 11:05:42 -0700

    updated Index

---
+++ src/Components/productsData.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+const productsData = [
+    { id: 1, name: 'Product A', price: 25000 },
+    { id: 2, name: 'Product B', price: 45000 },
+    { id: 3, name: 'Product C', price: 15000 },
+    { id: 4, name: 'Product D', price: 60000 }
+  ];
+ 
+export default productsData;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
 import { Col, Container, Row, Form, Card } from 'react-bootstrap';
 import cardImg from '../assets/model.jpg';
-
-// Sample product data
-const productsData = [
-  { id: 1, name: 'Product A', price: 25000 },
-  { id: 2, name: 'Product B', price: 45000 },
-  { id: 3, name: 'Product C', price: 15000 },
-  { id: 4, name: 'Product D', price: 60000 }
-];
+import productsData from '../components/productsData'
 
 // Card Component
 const ProductCard = ({ name, price }) => {
